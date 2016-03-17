6fa477067a1291762076da2eb70a875f22522080
陈希章 <chen_xizhang@hotmail.com>
2016-03-17 23:19:47 +0800

    138个错误

---
+++ xmldatabase-coreclr/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("xmldatabase_coreclr")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("xmldatabase_coreclr")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("218d8e4e-74a1-45d8-ab4a-482750ba1c65")]
---
+++ xmldatabase-coreclr/XUtility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XmlDatabase.Core
+{
+    class XUtility
+    {
+        private XUtility() { }
+        public static string EngineVersion {
+            get {
+                return typeof(XUtility).Assembly.GetName().Version.ToString();
+            }
+        }
+    }
+}
